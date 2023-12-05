7f8906e7d880ee2bf5bd1bcc42485b6e84d3789d
franconero14 <nerobalor@gmail.com>
2023-12-05 22:23:11 +0700

    Add FormInputSiswa component

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanEntry.kt
@@ -1,5 +1,6 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import android.widget.NumberPicker.OnValueChangeListener
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -7,6 +8,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.DetailSiswa
 import com.example.roomsiswa.model.EntryViewModel
 import com.example.roomsiswa.model.PenyediaViewModel
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
@@ -26,3 +28,11 @@ fun EntrySiswaScreen(
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    detailSiswa: DetailSiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailSiswa) -> Unit = {}
+){}
