dbcc0e67ad3c69b25e63d43ef47c7eceacf6cfd1
franconero14 <nerobalor@gmail.com>
2023-12-05 19:22:36 +0700

    Create data class HomeUiState in HomeViewModel

--- app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.roomsiswa.model
 
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
     companion object{
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
