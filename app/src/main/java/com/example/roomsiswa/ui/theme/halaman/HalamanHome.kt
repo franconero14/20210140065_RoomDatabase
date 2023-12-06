a4aa67531783af7950b7d8198b854b7fd02834d3
franconero14 <nerobalor@gmail.com>
2023-12-06 13:44:18 +0700

    Add DataSiswa component with Column layout

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
@@ -1,9 +1,13 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import com.example.roomsiswa.R
 import com.example.roomsiswa.data.Siswa
@@ -23,6 +27,11 @@ fun DataSiswa(
         modifier = modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
 
+        }
     }
 }
