5ee236dd34ec4557c28b9a08eabd493a5e1e3e28
franconero14 <nerobalor@gmail.com>
2023-12-06 13:39:14 +0700

    Add DataSiswa composable function

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
@@ -1,9 +1,18 @@
 package com.example.roomsiswa.ui.theme.halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.roomsiswa.R
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){}
