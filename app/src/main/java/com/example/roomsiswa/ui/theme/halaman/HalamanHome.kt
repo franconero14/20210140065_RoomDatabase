a4165fe621b1f2ba90965c9483a05e2ccf50b454
franconero14 <nerobalor@gmail.com>
2023-12-06 13:58:48 +0700

    Add HomeScreen composable function

--- app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/halaman/HalamanHome.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,14 +23,25 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
 import com.example.roomsiswa.data.Siswa
+import com.example.roomsiswa.model.HomeViewModel
+import com.example.roomsiswa.model.PenyediaViewModel.Factory
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = Factory)
+){}
 
 @Composable
 fun BodyHome(
